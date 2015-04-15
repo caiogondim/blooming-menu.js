90ddef2d2e5efc9913d442cd7c74edd7feeefaf3
Caio Gondim <me@caiogondim.com>
2015-04-15 13:53:32 +0200

    Renames built file to blooming-menu.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
 
 gulp.task('build', function () {
   return gulp.src('src/*')
     .pipe(uglify())
+    .pipe(rename('blooming-menu.min.js'))
     .pipe(gulp.dest('build'))
 })
